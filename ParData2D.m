cc71b6002005cfa6844b1b54baff3079923c491f
HansenZhao <zhaohs12@mails.tsinghua.edu.cn>
2017-08-22 16:52:40 +0800

    init

---
+++ ParData2D.m
@@ -0,0 +1,16 @@
+classdef ParData2D
+    %ParData2D Summary of this class goes here
+    %   particle data container
+    %   raw data should be in standard format n-by-4 matrix
+    %   |particle_id|frame|x|y|
+    
+    properties
+    end
+    properties(Access = private)
+        rawMat;
+    end
+    methods
+    end
+    
+end
+

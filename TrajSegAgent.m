0332e54d1290cbf5efabe1a8abc4e35fcc369377
HansenZhao <zhaohs12@mails.tsinghua.edu.cn>
2017-08-25 21:11:18 +0800

    add segLength field,fix bug in small segment

--- TrajSegAgent.m
+++ TrajSegAgent.m
@@ -15,26 +15,36 @@ classdef TrajSegAgent < handle
         deltaT;
     end
     
+    properties(Dependent)
+        segLength;
+    end
+    
     methods
         function obj = TrajSegAgent(seg,frame,gridPos,deltaT,varargin)
-            if isempty(varargin)
-                lag = 20; maxP = 6;
-            elseif nargin == 5
-                lag = varargin{1}; maxP = 6;
-            elseif nargin == 6
-                lag = varargin{1}; maxP = varargin{2};
-            end
             obj.traj = seg;
             obj.frame = frame;
             obj.gridPos = gridPos;
             obj.deltaT = deltaT;
             obj.aveVel = mean(TrajAnalysis2D.xy2vel(seg,obj.deltaT,0));
             obj.dir = seg(end,:) - seg(1,:);
+            
+            if isempty(varargin)
+                lag = min(20,floor(obj.segLength/3)); maxP = 6;
+            elseif nargin == 5
+                maxP = varargin{1}; lag = min(20,floor(obj.segLength/3));
+            elseif nargin == 6
+                maxP = varargin{1}; lag = varargin{2};
+            end
+            
             obj.msdCurve = [eps,TrajAnalysis2D.xy2msd(seg,lag,2)];
             t = [eps,(1:1:lag)*obj.deltaT];
             [obj.alpha,obj.D] = TrajAnalysis2D.fitMSDCurve(t,obj.msdCurve,0);
             obj.Smss = TrajAnalysis2D.xy2mss(seg,lag,maxP,0);  
             obj.aysm = TrajAnalysis2D.xy2asym(seg);
+        end
+        
+        function sL = get.segLength(obj)
+            sL = size(obj.traj,1);
         end
     end
     

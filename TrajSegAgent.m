eaa198390a71931b6730c8c2f019f9b5f86877bc
HansenZhao <zhaohs12@mails.tsinghua.edu.cn>
2017-08-26 15:53:00 +0800

    process in short length

--- TrajSegAgent.m
+++ TrajSegAgent.m
@@ -28,19 +28,31 @@ classdef TrajSegAgent < handle
             obj.aveVel = mean(TrajAnalysis2D.xy2vel(seg,obj.deltaT,0));
             obj.dir = seg(end,:) - seg(1,:);
             
-            if isempty(varargin)
-                lag = min(20,floor(obj.segLength/3)); maxP = 6;
-            elseif nargin == 5
-                maxP = varargin{1}; lag = min(20,floor(obj.segLength/3));
-            elseif nargin == 6
-                maxP = varargin{1}; lag = varargin{2};
+            if obj.segLength > 6           
+                if isempty(varargin)
+                    lag = min(20,floor(obj.segLength/2)); maxP = 6;
+                elseif nargin == 5
+                    maxP = varargin{1}; lag = min(20,floor(obj.segLength/3));
+                elseif nargin == 6
+                    maxP = varargin{1}; lag = varargin{2};
+                end
+
+                obj.msdCurve = [eps,TrajAnalysis2D.xy2msd(seg,lag,2)];
+                t = [eps,(1:1:lag)*obj.deltaT];
+                [obj.alpha,obj.D] = TrajAnalysis2D.fitMSDCurve(t,obj.msdCurve,0);
+                obj.Smss = TrajAnalysis2D.xy2mss(seg,lag,maxP,0);  
+            else
+                obj.msdCurve = nan;
+                obj.alpha = nan;
+                obj.D = nan;
+                obj.Smss = nan;
             end
             
-            obj.msdCurve = [eps,TrajAnalysis2D.xy2msd(seg,lag,2)];
-            t = [eps,(1:1:lag)*obj.deltaT];
-            [obj.alpha,obj.D] = TrajAnalysis2D.fitMSDCurve(t,obj.msdCurve,0);
-            obj.Smss = TrajAnalysis2D.xy2mss(seg,lag,maxP,0);  
-            obj.aysm = TrajAnalysis2D.xy2asym(seg);
+            if obj.segLength > 3
+                obj.aysm = TrajAnalysis2D.xy2asym(seg);
+            else
+                obj.aysm = nan;
+            end
         end
         
         function sL = get.segLength(obj)

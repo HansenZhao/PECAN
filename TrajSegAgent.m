1a8a4cc71a3ef41f1cb5368a36a7de55808ac564
HansenZhao <zhaohs12@mails.tsinghua.edu.cn>
2017-08-28 10:40:48 +0800

    split the process of init and calculation

--- TrajSegAgent.m
+++ TrajSegAgent.m
@@ -13,6 +13,8 @@ classdef TrajSegAgent < handle
         gridPos;
         frame;
         deltaT;
+        lag;
+        maxP;
     end
     
     properties(Dependent)
@@ -25,22 +27,27 @@ classdef TrajSegAgent < handle
             obj.frame = frame;
             obj.gridPos = gridPos;
             obj.deltaT = deltaT;
-            obj.aveVel = mean(TrajAnalysis2D.xy2vel(seg,obj.deltaT,0));
-            obj.dir = seg(end,:) - seg(1,:);
-            
+            if isempty(varargin)
+                obj.lag = min(20,floor(obj.segLength/2)); obj.maxP = 6;
+            elseif nargin == 5
+                obj.maxP = varargin{1}; obj.lag = min(20,floor(obj.segLength/3));
+            elseif nargin == 6
+                obj.maxP = varargin{1}; obj.lag = varargin{2};
+            end
+        end
+        
+        function sL = get.segLength(obj)
+            sL = size(obj.traj,1);
+        end
+        
+        function calSelf(obj)
+            obj.aveVel = mean(TrajAnalysis2D.xy2vel(obj.traj,obj.deltaT,0));
+            obj.dir = obj.traj(end,:) - obj.traj(1,:);
             if obj.segLength > 6           
-                if isempty(varargin)
-                    lag = min(20,floor(obj.segLength/2)); maxP = 6;
-                elseif nargin == 5
-                    maxP = varargin{1}; lag = min(20,floor(obj.segLength/3));
-                elseif nargin == 6
-                    maxP = varargin{1}; lag = varargin{2};
-                end
-
-                obj.msdCurve = [eps,TrajAnalysis2D.xy2msd(seg,lag,2)];
-                t = [eps,(1:1:lag)*obj.deltaT];
+                obj.msdCurve = [eps,TrajAnalysis2D.xy2msd(obj.traj,obj.lag,2)];
+                t = [eps,(1:1:obj.lag)*obj.deltaT];
                 [obj.alpha,obj.D] = TrajAnalysis2D.fitMSDCurve(t,obj.msdCurve,0);
-                obj.Smss = TrajAnalysis2D.xy2mss(seg,lag,maxP,0);  
+                %obj.Smss = TrajAnalysis2D.xy2mss(seg,obj.lag,obj.maxP,0);  
             else
                 obj.msdCurve = nan;
                 obj.alpha = nan;
@@ -49,14 +56,10 @@ classdef TrajSegAgent < handle
             end
             
             if obj.segLength > 3
-                obj.aysm = TrajAnalysis2D.xy2asym(seg);
+                obj.aysm = TrajAnalysis2D.xy2asym(obj.traj);
             else
                 obj.aysm = nan;
             end
-        end
-        
-        function sL = get.segLength(obj)
-            sL = size(obj.traj,1);
         end
     end
     

1919b4b96ddf84b018a8bcf9f7c95379abb4f075
HansenZhao <zhaohs12@mails.tsinghua.edu.cn>
2018-01-30 11:19:59 +0800

    change colormap

--- GlobalConfig.m
+++ GlobalConfig.m
@@ -3,7 +3,9 @@ classdef GlobalConfig
     %   Detailed explanation goes here
     
     properties(Constant)
-        cmap = jet;
+        %cmap = jet;
+        cmap = [repmat(linspace(0.3,0.8,32)',1,2),linspace(1,0.8,32)';...
+                linspace(0.8,1,32)',repmat(linspace(0.8,0.3,32)',1,2)];
     end
     
     methods

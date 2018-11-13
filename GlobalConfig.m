7f5a6d7bde8940ff80c5a8a6b226be8a83fa61c9
HansenZhao <zhaohs12@mails.tsinghua.edu.cn>
2018-11-13 19:45:35 +0800

    cmap

--- GlobalConfig.m
+++ GlobalConfig.m
@@ -3,9 +3,13 @@ classdef GlobalConfig
     %   Detailed explanation goes here
     
     properties(Constant)
-        %cmap = jet;
-        cmap = [repmat(linspace(0.3,0.8,32)',1,2),linspace(1,0.8,32)';...
-                linspace(0.8,1,32)',repmat(linspace(0.8,0.3,32)',1,2)];
+        %default colormap
+        cmap = jet;
+        %matlab default color map
+        %cmap = parula;
+        %red-blue color map
+        %cmap = [repmat(linspace(0.3,0.8,32)',1,2),linspace(1,0.8,32)';...
+                %linspace(0.8,1,32)',repmat(linspace(0.8,0.3,32)',1,2)];
     end
     
     methods

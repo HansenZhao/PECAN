7aa2438b4f9b961aea554cf99ae739b2b51c4cc2
HansenZhao <zhaohs12@mails.tsinghua.edu.cn>
2018-11-13 19:46:32 +0800

    row names

--- HScsvwrite.m
+++ HScsvwrite.m
@@ -1,12 +1,24 @@
-function [] = HScsvwrite(fileName,M,varargin)
+function [] = HScsvwrite(fileName,M,strId,varargin)
     fid = fopen(fileName,'a');
+    if isempty(strId)
+        isStr = false;
+    else
+        isStr = true;
+    end
     [R,L] = size(M);
     if ~isempty(varargin)
         header = varargin{1};
         fprintf(fid,[header,'\n']);
     end
     strLine = repStrline('%f,',L);
+    if isStr
+        idNum = size(strId,2);
+        strFormat = repmat('%s,',idNum);
+    end
     for m = 1:1:R
+        if isStr
+            fprintf(fid,strFormat,strId{m,:});
+        end
         fprintf(fid,strLine,M(m,:));
     end
     fclose(fid);
